34b830a8cfd9b1f2dfef716c070feab960dd4dac
Anshaj Vats <9931633906vats@gmail.com>
2025-02-20 03:49:56 -0800

    Corrected the image import for geoart

--- application/frontend/src/assets/index.ts
+++ application/frontend/src/assets/index.ts
@@ -2,5 +2,5 @@ export { default as anshajImg } from "./members/profilePic.jpg";
 export { default as francisImg } from "./members/FrancisAviles.jpg";
 export { default as ayeshaImg } from "./members/AyeshaIrum.jpg";
 export { default as kyleImg } from "./members/KyleNguyen.jpg";
-export { default as geoartImg } from "./members/GeoartCorral.JPEG";
+export { default as geoartImg } from "./members/GeoartCorral.jpg";
 export { default as karlaImg } from "./members/KarlaCardenas.jpg";
