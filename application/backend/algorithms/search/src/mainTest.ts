2a9b9d7a5fb3c5cca3a38df2450c289759166ac8
SourMayo <geoartcorral@gmail.com>
2025-03-24 15:58:32 -0700

    Completed simple search

---
+++ application/backend/algorithms/search/src/mainTest.ts
@@ -0,0 +1,24 @@
+  import * as search from "./utils/search"
+
+async function main() {
+    console.log("Testing Search In Test Program");
+
+
+    // Display all items in database
+    console.log("Select All:");
+    const location = await search.showAll();
+    console.log(location);
+
+
+    // Display resources with "place" it its name
+    console.log("\n\Simple Search Test:")
+    
+    // TODO: userInput should take in value from website
+    let userInput:string = "place"
+
+    const searchTest = await search.simpleSearch(userInput);
+    console.log(searchTest);
+
+}
+
+main();
