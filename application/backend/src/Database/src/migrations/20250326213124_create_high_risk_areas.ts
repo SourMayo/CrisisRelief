db82e4a7309eedb83ddbf87239ba8ada0b528efb
AviGhome <99893272+AviGeck@users.noreply.github.com>
2025-04-15 21:03:22 -0700

    Database migrations adjustment

--- application/backend/src/Database/src/migrations/20250326213124_create_high_risk_areas.ts
+++ application/backend/src/Database/src/migrations/20250326213124_create_high_risk_areas.ts
@@ -4,18 +4,18 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("high_risk_areas", (table) => {
     table.increments("area_id").primary();
     table
-      .integer("location_id")
-      .notNullable()
-      .references("location_id")
-      .inTable("locations");
+    .integer("location_id")
+    .notNullable()
+    .references("location_id")
+    .inTable("locations");
     table.text("description");
     table.text("safe_zones");
-
+    
     table.string("street");
     table.string("city");
     table.string("state",2);
     table.string("zipcode",20);
-
+    
     table.float("latitude", 9,6);
     table.float("longitude", 9,6);
     table.integer("radius_miles");
