e2567bfea5b364fcc98537a9025aa2df6c51c8ae
Ayesha Irum <ayeshairum@Mac.attlocal.net>
2025-05-08 06:37:16 -0700

    It was just testing template, feel free to not use it

--- application/frontend/src/main.tsx
+++ application/frontend/src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ThemeProvider } from "./context/ThemeContext"; // ✅ Add this
+import { ThemeProvider } from "./context/ThemeContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       {" "}
-      {/* ✅ Wrap App */}
+      {/* sWrap App */}
       <App />
     </ThemeProvider>
   </StrictMode>
