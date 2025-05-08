b6bfa6340427bd1c44e2316600d3c2ef0f73a899
SourMayo <geoartcorral@gmail.com>
2025-05-08 14:05:58 -0700

    Fixed vite.config.ts

--- application/frontend/vite.config.ts
+++ application/frontend/vite.config.ts
@@ -2,4 +2,11 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  server: {
+    host: true, // Listen on all interfaces (0.0.0.0)
+    port: 5173,
+    allowedHosts: ['crisisrelief.duckdns.org'], 
+
+    cors: true,
+  },
 });
